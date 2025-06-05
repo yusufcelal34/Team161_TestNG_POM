693f39a592ba8ebeb936fbf75fe338fb6ce4e5dd
Yusuf Celal <yusufcelal34@gmail.com>
2025-06-05 22:48:12 +0300

    05.06.2025-22:48

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -16,10 +16,10 @@ import java.util.Set;
 
 public class ReusableMethods {
 
-    public static void bekle(int saniye)  {
+    public static void bekle(int saniye) {
 
         try {
-            Thread.sleep(saniye*1000);
+            Thread.sleep(saniye * 1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -32,11 +32,11 @@ public class ReusableMethods {
     // tum elementler elden gectikten sonra
     // String'lerden olusan listeyi DONDUREN bir method olusturun
 
-    public static List<String> stringListeDonustur (List<WebElement> webElementList){
+    public static List<String> stringListeDonustur(List<WebElement> webElementList) {
 
         List<String> stringList = new ArrayList<>();
 
-        for (WebElement eachElement:webElementList){
+        for (WebElement eachElement : webElementList) {
             stringList.add(eachElement.getText());
 
         }
@@ -45,41 +45,41 @@ public class ReusableMethods {
 
     }
 
-    public static void urlIleWindowGecisi(WebDriver driver, String hedefUrl){
+    public static void urlIleWindowGecisi(WebDriver driver, String hedefUrl) {
 
         Set<String> acikOlanTumWindowlarinWhdSeti = driver.getWindowHandles();
 
-        for (String eachWhd :acikOlanTumWindowlarinWhdSeti){
+        for (String eachWhd : acikOlanTumWindowlarinWhdSeti) {
 
             driver.switchTo().window(eachWhd);
 
             String actualUrl = driver.getCurrentUrl();
 
-            if (hedefUrl.equals(actualUrl)){
+            if (hedefUrl.equals(actualUrl)) {
                 break;
             }
 
         }
     }
 
-    public static void titleIleWindowGecisi(WebDriver driver, String hedefTitle){
+    public static void titleIleWindowGecisi(WebDriver driver, String hedefTitle) {
 
         Set<String> acikOlanTumWindowlarinWhdSeti = driver.getWindowHandles();
 
-        for (String eachWhd :acikOlanTumWindowlarinWhdSeti){
+        for (String eachWhd : acikOlanTumWindowlarinWhdSeti) {
 
             driver.switchTo().window(eachWhd);
 
             String actualTitle = driver.getTitle();
 
-            if (hedefTitle.equals(actualTitle)){
+            if (hedefTitle.equals(actualTitle)) {
                 break;
             }
 
         }
     }
 
-    public static void tumSayfaResimCek(WebDriver driver){
+    public static void tumSayfaResimCek(WebDriver driver) {
         // 1.adim : TakesScreenshot objesi olustur
         TakesScreenshot tss = (TakesScreenshot) driver;
 
@@ -92,13 +92,13 @@ public class ReusableMethods {
         // 4.adim : geciciresim dosyasini, asil kaydetmek istedigimiz file'a kopyalayalim
 
         try {
-            FileUtils.copyFile(geciciResim,file);
+            FileUtils.copyFile(geciciResim, file);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void tumSayfaResimCek(WebDriver driver, String raporIsmi){
+    public static void tumSayfaResimCek(WebDriver driver, String raporIsmi) {
 
         try {
             Thread.sleep(500);
@@ -110,7 +110,7 @@ public class ReusableMethods {
         TakesScreenshot tss = (TakesScreenshot) driver;
 
         // 2.adim : screenshot'i kaydedecegimiz dosyayi (File) olusturun
-        File file = new File("target/screenshots/"+ raporIsmi + ".jpg");
+        File file = new File("target/screenshots/" + raporIsmi + ".jpg");
 
         // 3.adim : screenshot'i alip gecici bir dosyaya kaydedin
         File geciciResim = tss.getScreenshotAs(OutputType.FILE);
@@ -118,13 +118,13 @@ public class ReusableMethods {
         // 4.adim : geciciresim dosyasini, asil kaydetmek istedigimiz file'a kopyalayalim
 
         try {
-            FileUtils.copyFile(geciciResim,file);
+            FileUtils.copyFile(geciciResim, file);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void tumSayfaResimCekTarihli(WebDriver driver){
+    public static void tumSayfaResimCekTarihli(WebDriver driver) {
 
         try {
             Thread.sleep(500);
@@ -149,7 +149,7 @@ public class ReusableMethods {
         TakesScreenshot tss = (TakesScreenshot) driver;
 
         // 2.adim : screenshot'i kaydedecegimiz dosyayi (File) olusturun
-        File file = new File("target/screenshots/tumSayfaSs"+tarihEtiketi+".jpg");
+        File file = new File("target/screenshots/tumSayfaSs" + tarihEtiketi + ".jpg");
 
         // 3.adim : screenshot'i alip gecici bir dosyaya kaydedin
         File geciciResim = tss.getScreenshotAs(OutputType.FILE);
@@ -157,13 +157,13 @@ public class ReusableMethods {
         // 4.adim : geciciresim dosyasini, asil kaydetmek istedigimiz file'a kopyalayalim
 
         try {
-            FileUtils.copyFile(geciciResim,file);
+            FileUtils.copyFile(geciciResim, file);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void tumSayfaResimCekTarihli(WebDriver driver, String raporIsmi){
+    public static void tumSayfaResimCekTarihli(WebDriver driver, String raporIsmi) {
 
         try {
             Thread.sleep(500);
@@ -178,12 +178,11 @@ public class ReusableMethods {
         String tarihEtiketi = localDateTime.format(istenenFormat);
 
 
-
         // 1.adim : TakesScreenshot objesi olustur
         TakesScreenshot tss = (TakesScreenshot) driver;
 
         // 2.adim : screenshot'i kaydedecegimiz dosyayi (File) olusturun
-        File file = new File("target/screenshots/"+ raporIsmi + tarihEtiketi + ".jpg");
+        File file = new File("target/screenshots/" + raporIsmi + tarihEtiketi + ".jpg");
 
         // 3.adim : screenshot'i alip gecici bir dosyaya kaydedin
         File geciciResim = tss.getScreenshotAs(OutputType.FILE);
@@ -191,13 +190,13 @@ public class ReusableMethods {
         // 4.adim : geciciresim dosyasini, asil kaydetmek istedigimiz file'a kopyalayalim
 
         try {
-            FileUtils.copyFile(geciciResim,file);
+            FileUtils.copyFile(geciciResim, file);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void webElementResimCek(WebElement hedefElement){
+    public static void webElementResimCek(WebElement hedefElement) {
         // 1.adim : screenshot alacaginiz webelementi locate edip, kaydedin
         //          biz arama sonuc yazisi elementini zaten locate edip kaydetmistik
 
@@ -211,18 +210,18 @@ public class ReusableMethods {
 
         // 4.adim : gecici resmi asil resme kopyalayin
         try {
-            FileUtils.copyFile(geciciResim,asilResim);
+            FileUtils.copyFile(geciciResim, asilResim);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void webElementResimCek(WebElement hedefElement, String resimIsmi){
+    public static void webElementResimCek(WebElement hedefElement, String resimIsmi) {
         // 1.adim : screenshot alacaginiz webelementi locate edip, kaydedin
         //          biz arama sonuc yazisi elementini zaten locate edip kaydetmistik
 
         // 2.adim : screenshot'i kaydedecegimiz dosyayi(File) olusturun
-        String dosyaYolu = "target/screenshots/"+resimIsmi+".jpg";
+        String dosyaYolu = "target/screenshots/" + resimIsmi + ".jpg";
         File asilResim = new File(dosyaYolu);
 
         // 3.adim : webElement'i kullanarak screenshot alip
@@ -231,13 +230,13 @@ public class ReusableMethods {
 
         // 4.adim : gecici resmi asil resme kopyalayin
         try {
-            FileUtils.copyFile(geciciResim,asilResim);
+            FileUtils.copyFile(geciciResim, asilResim);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void webElementTarihliResimCek(WebElement hedefElement){
+    public static void webElementTarihliResimCek(WebElement hedefElement) {
 
         LocalDateTime localDateTime = LocalDateTime.now();
         // istedigimiz formati olusturalim _290525_190923
@@ -249,7 +248,7 @@ public class ReusableMethods {
         //          biz arama sonuc yazisi elementini zaten locate edip kaydetmistik
 
         // 2.adim : screenshot'i kaydedecegimiz dosyayi(File) olusturun
-        String dosyaYolu = "target/screenshots/istenenWebElement"+tarihEtiketi+".jpg";
+        String dosyaYolu = "target/screenshots/istenenWebElement" + tarihEtiketi + ".jpg";
         File asilResim = new File(dosyaYolu);
 
         // 3.adim : webElement'i kullanarak screenshot alip
@@ -258,13 +257,13 @@ public class ReusableMethods {
 
         // 4.adim : gecici resmi asil resme kopyalayin
         try {
-            FileUtils.copyFile(geciciResim,asilResim);
+            FileUtils.copyFile(geciciResim, asilResim);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void webElementTarihliResimCek(WebElement hedefElement, String resimIsmi){
+    public static void webElementTarihliResimCek(WebElement hedefElement, String resimIsmi) {
 
         LocalDateTime localDateTime = LocalDateTime.now();
         // istedigimiz formati olusturalim _290525_190923
@@ -276,7 +275,7 @@ public class ReusableMethods {
         // 1.adim : screenshot alacaginiz webelementi locate edip, kaydedin
 
         // 2.adim : screenshot'i kaydedecegimiz dosyayi(File) olusturun
-        String dosyaYolu = "target/screenshots/"+resimIsmi+tarihEtiketi+".jpg";
+        String dosyaYolu = "target/screenshots/" + resimIsmi + tarihEtiketi + ".jpg";
         File asilResim = new File(dosyaYolu);
 
         // 3.adim : webElement'i kullanarak screenshot alip
@@ -285,7 +284,7 @@ public class ReusableMethods {
 
         // 4.adim : gecici resmi asil resme kopyalayin
         try {
-            FileUtils.copyFile(geciciResim,asilResim);
+            FileUtils.copyFile(geciciResim, asilResim);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
